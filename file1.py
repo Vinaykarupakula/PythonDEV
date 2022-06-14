db7d055b661e7fac9eccaa79dd71fc232e26ce04
vinay Kumar Reddy Karupakula <vk21085@corp.ojas-it.com>
2022-06-14 11:44:03 +0530

    addded prime number logic

--- file1.py
+++ file1.py
@@ -1,3 +1,9 @@
 def prime(num):
-    pass
+    if num>2:
+        for i in range(2,num):
+            if num%2==0:
+                print(num, "is not a prime number")
+                break
+        else:
+            print(num, "is a prime number")
 prime(37)
