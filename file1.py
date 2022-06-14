68bf486167354f8da8460bcabdc5628858c7e115
vinay Kumar Reddy Karupakula <vk21085@corp.ojas-it.com>
2022-06-14 12:07:09 +0530

    Revert "corrected the logic with repplacing i instead of 2"
    This reverts commit b95a3ef4937ec2ceb3374c6c61e9719c39730278.

--- file1.py
+++ file1.py
@@ -1,14 +1,9 @@
 def prime(num):
-    if num==1:
-        print(1, "is a prime nuber")
-    elif num==2:
-        print(num, "is a even prime number")
-    else:
-        if num>2:
-            for i in range(2,num):
-                if num%i==0:
-                    print(num, "is not a prime number")
-                    break
-            else:
-                print(num, "is a prime number")
-prime(15)
+    if num>2:
+        for i in range(2,num):
+            if num%2==0:
+                print(num, "is not a prime number")
+                break
+        else:
+            print(num, "is a prime number")
+prime(37)
